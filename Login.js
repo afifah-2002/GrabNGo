f7e69fc6a4f669e95b7f5abcb50d65debf4886b6
Nithesh0812 <82301793+Nithesh0812@users.noreply.github.com>
2024-10-04 14:30:40 -0500

    Update Login.js

--- Login.js
+++ Login.js
@@ -30,7 +30,7 @@ const Login = () => {
             <li><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="orange" class="bi bi-justify-right" viewBox="0 0 16 18">
   <path fill-rule="evenodd" d="M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
 </svg></li>&nbsp;&nbsp;
-            <li> <h5 className="text-light">appname</h5></li>
+            <li> <h5 className="text-light">Grab N' Go</h5></li>
           </ul>
             </div>
           </div>
